9b5b75a4476c35d102d32c0dc71fdf992ad535e0
lubualo@gmail.com <lubualo@gmail.com>
2025-06-25 20:08:27 -0300

    Started the get for products

--- internal/product/handler.go
+++ internal/product/handler.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/lubualo/ecommerce-go/models"
@@ -76,10 +77,88 @@ func (h *Handler) Delete(request events.APIGatewayV2HTTPRequest) *events.APIGate
 }
 
 func (h *Handler) Get(request events.APIGatewayV2HTTPRequest) *events.APIGatewayProxyResponse {
-	return tools.CreateApiResponse(http.StatusMethodNotAllowed, "Not implemented")
-
+		return tools.CreateApiResponse(http.StatusMethodNotAllowed, "Not implemented")
 	// var categories []models.Category
 	// var err error
+    // query := request.QueryStringParameters
+
+	// idStr := query["id"]
+	// if idStr != "" {
+    // 	id, err := strconv.Atoi(idStr)
+	// 	if err != nil || id <= 0 {
+	// 		return tools.CreateApiResponse(http.StatusBadRequest, "Invalid 'id' parameter")
+	// 	}
+	// 	product, err := h.service.GetById(id)
+	// 	if err != nil {
+	// 		return tools.CreateApiResponse(http.StatusNotFound, "Product not found: " + err.Error())
+	// 	}
+	// 	body, err := json.Marshal(product)
+	// 	if err != nil {
+	// 		return tools.CreateApiResponse(http.StatusBadRequest, "Error converting to JSON: "+err.Error())
+	// 	}
+	// 	return tools.CreateApiResponse(http.StatusOK, string(body))
+	// }
+
+    // page := 1 // Default value
+	// value := query["page"]
+	// if value != "" {
+    //     parsed, err := strconv.Atoi(value)
+    //     if err != nil || parsed < 1 {
+    //         return tools.CreateApiResponse(http.StatusBadRequest, "Invalid 'page' parameter")
+    //     }
+	// 	page = parsed
+	// }
+
+    // limit := 10 // Default value
+	// value = query["limit"]
+	// if value != "" {
+    //     parsed, err := strconv.Atoi(value)
+    //     if err != nil || parsed < 1 {
+    //         return tools.CreateApiResponse(http.StatusBadRequest, "Invalid 'limit' parameter")
+    //     }
+    //     limit = parsed
+    // }
+
+    // sortBy := "id" // Default value
+	// value = query["sort_by"]
+	// if value != "" {
+    //     allowed := map[string]bool{
+    //         "id": true,
+    //         "title": true,
+    //         "description": true,
+    //         "price": true,
+    //         "category_id": true,
+    //         "stock": true,
+    //         "created_at": true,
+    //     }
+    //     if !allowed[value] {
+    //         return tools.CreateApiResponse(http.StatusBadRequest, "Invalid 'sort_by' parameter")
+    //     }
+    //     sortBy = value
+    // }
+	
+    // order := "ASC" // Default value
+	// value = query["order"]
+	// if value != "" {
+    //     upperCase := strings.ToUpper(value)
+    //     if upperCase != "ASC" && upperCase != "DESC" {
+    //         return tools.CreateApiResponse(http.StatusBadRequest, "Invalid 'order' parameter")
+    //     }
+    //     order = upperCase
+    // }
+
+    // products, err := h.service.GetAll(page, limit, sortBy, order)
+    // if err != nil {
+    //     return tools.CreateApiResponse(http.StatusInternalServerError, err.Error())
+    // }
+
+	// body, err := json.Marshal(products)
+	// if err != nil {
+	// 	return tools.CreateApiResponse(http.StatusBadRequest, "Error converting to JSON: "+err.Error())
+	// }
+	// return tools.CreateApiResponse(http.StatusOK, string(body))
+
+
 
 	// idStr := request.QueryStringParameters["id"]
 	// slug := request.QueryStringParameters["slug"]
